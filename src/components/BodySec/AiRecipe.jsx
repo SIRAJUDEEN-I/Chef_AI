97b60e2db8dd05c4a76e4c9634a0aa699325b699
SIRAJUDEEN-I <sirajudeen4300@gmail.com>
2025-06-10 10:06:38 +0530

    added responsivness for mobile

--- src/components/BodySec/AiRecipe.jsx
+++ src/components/BodySec/AiRecipe.jsx
@@ -15,13 +15,13 @@ export default function AiRecipe(props) {
 
     return (
         <section>
-            <h2 className="text-3xl font-bold text-[#141413] mt-10">Your Recipe:</h2>
+            <h2 className="text-2xl px-2 pb-2 md:text-3xl font-bold text-[#141413] mt-10">Your Recipe:</h2>
             <div
                 ref={recipeRef}
-                className="recipe-content mt-5 mb-10 p-5 bg-gray-100 rounded-lg  focus:outline-none focus:ring-2 focus:ring-blue-400"
+                className="recipe-content mt-3 mb-10 p-5 bg-gray-100 rounded-lg  focus:outline-none focus:ring-2 focus:ring-blue-400"
                 tabIndex={-1}
             >
-                <h2 className="text-2xl font-bold mb-3 pl-10">Recipe Instructions</h2>
+                <h2 className="text-2xl font-bold mb-3 pl-auto">Recipe Instructions</h2>
                 <FadeContent>
                 <p className="text-lg">
                     <Markdown>{props.recipe}</Markdown>
--- src/components/BodySec/BodySec.jsx
+++ src/components/BodySec/BodySec.jsx
@@ -13,7 +13,7 @@ export default function BodySec(){
    const [recipe, setRecipe] = useState("")
 
    const ingredientsListItems = ingredients.map(ingredient => (
-        <li key={ingredient}>{ingredient}</li>
+        <li  key={ingredient}>{ingredient}</li>
     ))
 
     
@@ -34,28 +34,36 @@ export default function BodySec(){
     }
     return (
         
-        <main className="body-sec flex flex-col items-center justify-center mt-10">
-            <h1 className="text-3xl font-bold text-[#141413]">What ingredients do you have? on mind </h1>
+        <main className="body-sec flex flex-col items-center justify-center mt-10 px-4 sm:px-8 md:px-16">
+            <h1 className="text-2xl sm:text-3xl font-bold text-[#141413] text-center">What ingredients do you have? on mind </h1>
 
-            <form action={addIngredient} className="add-ingredient-form flex justify-center gap-[12px] h-[38px] mt-10">
-                <input type="text" name="ingredient" placeholder="    eg. Oregano" className="  pl-3 border-dashed border-2 border-[#D1D5DB] rounded mr-4 transition  focus:scale-110"/>
-                <button className=" bg-[#141413] font-medium text-white rounded-3xl pl-5 pr-5 transition hover:scale-105  ">+ Add Ingredients to the list</button>
+             <form action={addIngredient} className="add-ingredient-form flex flex-col sm:flex-row justify-center gap-4 h-auto sm:h-[38px] mt-10 w-full  max-w-xl">
+                <input
+                    type="text"
+                    name="ingredient"
+                    placeholder="eg. Oregano"
+                    className="pl-3 border-dashed border-2 border-[#D1D5DB] rounded transition focus:scale-110 py-2 ml-3 mr-3 sm:w-80 w-auto lg:py-5"
+                />
+                <button className="bg-[#141413] font-medium text-white rounded-3xl py-2 ml-auto mr-auto transition hover:scale-105 w-60  ">
+                    + Add Ingredients to the list
+                </button>
             </form>
           
             {ingredients.length > 0 &&
-                 <section className="motion-preset-slide-down">
-                <h2 className="text-3xl font-bold text-[#141413]} pr-30 mt-7">Ingredients on hand:</h2>
-                <ul className="mt-10 mr-70 pl-5 mb-10 list-disc">{ingredientsListItems}</ul>
+                <section className="motion-preset-slide-down w-full max-w-2xl">
+                    <h2 className="text-xl sm:text-3xl font-bold text-[#141413] mt-7">Ingredients on hand:</h2>
+                    <ul className="mt-5 pl-5 mb-10 list-disc text-[20px] md:text-2xl">{ingredientsListItems}</ul>
 
-                <div className="bg-gray-200  flex flex-col rounded-2xl pr-50">
-                    <h3 className=" mt-5 text-2xl pl-5 font-bold">Ready for a Recipe?</h3>
-                    <p className="p-5 pb-0 mb-0">generate a recipe with your ingredients.</p>
-                    <button className="border    ml-50 mt-3 p-0 m-0 mb-5 bg-amber-400 transition-transform focus:scale-90 hover:scale-110 focus:ring-2 rounded-2xl focus:bg-amber-300" onClick={getRecipe}>Get a Recipe</button>
-                </div>
-                
+                    <div className="bg-gray-200 flex flex-col rounded-2xl p-5">
+                        <h3 className="mt-2 text-lg sm:text-2xl font-bold">Ready for a Recipe?</h3>
+                        <p className="mb-2">Generate a recipe with your ingredients.</p>
+                        <button className="border mt-3 bg-amber-400 transition-transform focus:scale-90 hover:scale-110 focus:ring-2 rounded-2xl focus:bg-amber-300  ml-auto  mr-auto px-5 py-[3px] md:ml-55 md:w-40 m" onClick={getRecipe}>
+                            Get a Recipe
+                        </button>
+                    </div>
                 </section>
             }
-                  {recipe &&  <AiRecipe recipe={recipe} />}
+            {recipe && <AiRecipe recipe={recipe} />}
         </main>
         
     )
